5a48519723541cd7767b535063671a0366272b09
NichakornPansai <153910047+NichakornPansai@users.noreply.github.com>
2023-12-15 15:48:05 +0100

    Delete std2hom.m

--- std2hom.m
+++
@@ -1,8 +0,0 @@
-function hom_points = std2hom(points)
-    % points must contain each
-
-    % convert in homogeneous coordinates
-    hom_points = [points; ones( 1, size(points,2) )];   % homogeneous
-
-
-end
